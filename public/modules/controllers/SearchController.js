46eb5839dc49a69df044605d0ef3c7bc8f37fa0c
Kyle Chadha <kyle.chadha@buzzfeed.com>
2014-12-07 22:48:37 -0500

    Add CSS3 loading spinner

--- public/modules/controllers/SearchController.js
+++ public/modules/controllers/SearchController.js
@@ -3,15 +3,15 @@
   var SearchController = function($scope, $log, searchFactory) {
 
     // Reminder: Before you finish, pull the repo down from BitBucket in a clean folder and check everything (install scripts, phantomjs) etc., all work out of the box :)
+    // And add comments
 
     $scope.siteURL = 'http://'
     $scope.validURL = false;
-    $scope.errorMessage = '';
+    $scope.showSpinner = false;
 
     $scope.validateURL = function() {
       if (!$scope.siteURL.match(/^(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)) {
         $scope.validURL = false;
-        // $scope.errorMessage = 'Checking URL...';
       }
       else {
         $scope.validURL = true;
@@ -21,15 +21,18 @@
 
     $scope.getSiteInfo = function() {
 
-      // $scope.validateURL();
-
       if ($scope.siteURL && $scope.validURL) {
-        $log.log('Sending URL to factory.')
+
+        $scope.searchResults = '';
+        $scope.showSpinner = true;
+
         searchFactory.checkVersion($scope.siteURL, function(error, data) {
           if (!error) {
             $scope.searchResults = data.version;
           }
+          $scope.showSpinner = false;
         });
+        
       }
       else {
         $scope.searchResults = '';
