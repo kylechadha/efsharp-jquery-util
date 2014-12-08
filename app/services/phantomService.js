7521de382fab6579b3a98fc5c6e23b8383752af9
Kyle Chadha <kyle.chadha@buzzfeed.com>
2014-12-07 19:15:26 -0500

    Refine search view and styles

--- app/services/phantomService.js
+++ app/services/phantomService.js
@@ -26,7 +26,6 @@ module.exports = function(url, callback) {
             return ph.exit();
           });
         }
-
         
       });
     });
--- public/modules/controllers/SearchController.js
+++ public/modules/controllers/SearchController.js
@@ -5,32 +5,37 @@
     // Reminder: Before you finish, pull the repo down from BitBucket in a clean folder and check everything (install scripts, phantomjs) etc., all work out of the box :)
 
     $scope.siteURL = 'http://'
+    $scope.validURL = false;
+    $scope.errorMessage = '';
+
+    $scope.validateURL = function() {
+      if (!$scope.siteURL.match(/^(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)) {
+        $scope.validURL = false;
+        // $scope.errorMessage = 'Checking URL...';
+      }
+      else {
+        $scope.validURL = true;
+        $scope.errorMessage = '';
+      }
+    }
 
     $scope.getSiteInfo = function() {
 
-      $scope.errorMessage = '';
+      // $scope.validateURL();
 
-      if ($scope.siteURL) {
-
-        if (!$scope.siteURL.match(/^(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)) {
-          $scope.errorMessage = 'Invalid URL!';
-        }
-        else {
-
-          searchFactory.checkVersion($scope.siteURL, function(error, data) {
-            if (!error) {
-              $scope.errorMessage = data.version;
-
-            }
-          });
-
-        }
-
+      if ($scope.siteURL && $scope.validURL) {
+        $log.log('Sending URL to factory.')
+        searchFactory.checkVersion($scope.siteURL, function(error, data) {
+          if (!error) {
+            $scope.errorMessage = data.version;
+          }
+        });
+      }
+      else {
+        $scope.errorMessage = 'Whoops, that looks like an invalid URL.';
       }
 
-
     }
-
 
   };
 
